234c589c7e7e486e50be6dad5ae57520c256edee
Jennifer Chan <jenniferrxchan@gmail.com>
2017-09-13 23:31:57 -0400

    Update main.js

--- code/main.js
+++ code/main.js
@@ -2,12 +2,29 @@ function main(){
   $('.dropdown').hide();
   $('.links').hide();
   $('.button').on('click', function() {
+
+    if ($(window).width() > 1300) {
+      $(this).toggleClass("fourcol");
+      $(this).next().toggleClass("fourcol");
+    } else if ($(window).width() > 700) {
+      $(this).toggleClass("twocol");
+      $(this).find(".dropdown").toggleclass("twocol");
+    } else {
+
+      $(this).next().slideToggle(400);
+      $(this).toggleClass("onecol");
+      $(".headshot").toggleClass("transparent");
+      $(this).parent().toggleClass("fullborder");
+      $(this).next().toggleClass("mobclick");
+
+    }
+    /*
     //$(this).next().toggle();
     $(this).next().slideToggle(400);
-    $(this).toggleClass('active');
-    $(this).hover(function() {
-      $(this).hovertext("LinkedIn")
-    });
+    $(this).toggleClass('active');*/
+    /*$(this).hover(function() {
+      $(this).hovertext("LinkedIn");
+    });*/
   });
   $('.skillset').fadeIn(1000);
 }
